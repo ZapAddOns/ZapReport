5dd3721c0bd2e6e0a2590b80d3c82bd24d2edf72
Dirk Weltz <130468140+DirkWeltz@users.noreply.github.com>
2025-03-19 22:17:02 +0100

    Add possibility to use a list of dates to select files. This is important when a makeup fraction is created that isn't treated at the same day.

--- Logs/LogEntries.cs
+++ Logs/LogEntries.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using ZapReport.Helpers;
 
 namespace ZapReport.Objects
 {
@@ -27,12 +28,24 @@ namespace ZapReport.Objects
 
         public List<string> PlanNames { get => _logEntries.Keys.ToList(); }
 
-        public void CreateLogEntries(string planName, DateTime startTime, DateTime endTime)
+        public void CreateLogEntries(string planName, List<(DateTime, DateTime)> dates)
         {
+            var text = string.Empty;
+
+            foreach (var date in dates)
+            {
+                if (text == string.Empty)
+                    text = $"Create log entries for plan {planName} from {date.Item1} to {date.Item2}";
+                else
+                    text += $", from {date.Item1} to {date.Item2}";
+            }
+
+            _logger.Info(text);
+
             // Clear all log entries in case this function is called again
             _logEntries = new Dictionary<string, List<LogFractionEntry>>();
 
-            var fraction = GetNextFraction(planName, startTime, endTime);
+            var fraction = GetNextFraction(planName, dates);
 
             while (fraction != null)
             {
@@ -46,7 +59,7 @@ namespace ZapReport.Objects
                     _logEntries[fraction.PlanName].Add(fraction);
                 }
 
-                fraction = GetNextFraction(planName, startTime, endTime);
+                fraction = GetNextFraction(planName, dates);
             }
 
             UpdateIsocenterValues();
@@ -79,24 +92,28 @@ namespace ZapReport.Objects
             return result;
         }
 
-        public List<LogFractionEntry> GetEntriesForPlanAndDate(string planName, DateTime date)
+        public List<LogFractionEntry> GetEntriesForPlanAndDate(string planName, List<(DateTime, DateTime)> dates)
         {
-            var startDate = date;
-            var endDate = date.AddDays(1);
             var result = new List<LogFractionEntry>();
 
-            if (!_logEntries.ContainsKey(planName))
+            foreach (var date in dates)
             {
-                _logger.Log(LogLevel.Info, $"No log entries found for plan \"{planName}\"");
-
-                return result; 
-            }
-
-            foreach (var entry in _logEntries[planName])
-            {
-                if (entry.Time > startDate && entry.Time < endDate && entry != null)
+                if (!_logEntries.ContainsKey(planName))
                 {
-                    result.Add(entry);
+                    _logger.Log(LogLevel.Info, $"No log entries found for plan \"{planName}\"");
+
+                    return result;
+                }
+
+                var startDate = date.Item1;
+                var endDate = date.Item2;
+
+                foreach (var entry in _logEntries[planName])
+                {
+                    if (entry.Time > startDate && entry.Time < endDate && entry != null)
+                    {
+                        result.Add(entry);
+                    }
                 }
             }
 
@@ -115,7 +132,7 @@ namespace ZapReport.Objects
                 {
                     foreach (var isocenter in fraction.Isocenters)
                     {
-                        foreach (var beam in isocenter.Value.Beams)
+                        foreach (var beam in isocenter.Beams)
                         {
                             if (beam.Node < lastBeamNum)
                             {
@@ -137,14 +154,14 @@ namespace ZapReport.Objects
             }
         }
 
-        private LogFractionEntry GetNextFraction(string planName, DateTime startTime, DateTime endTime)
+        private LogFractionEntry GetNextFraction(string planName, List<(DateTime, DateTime)> dates)
         {
             LogFractionEntry fraction;
             LogIsocenterEntry isocenter = null;
             Match match;
             int pos = 0;
 
-            var line = GetNextLine();
+            var line = GetNextLine(dates);
             pos++;
 
             while (line != null)
@@ -153,13 +170,13 @@ namespace ZapReport.Objects
                 {
                     match = LogRegEx.RegexFractionStart.Match(line);
 
-                    if (match.Groups[3].Value == planName && CheckDate(match.Groups[1].Value, match.Groups[2].Value, startTime, endTime))
+                    if (match.Groups[3].Value == planName && Utilities.CheckDate(match.Groups[1].Value, match.Groups[2].Value, dates))
                     { 
                         break; 
                     }
                 }
 
-                line = GetNextLine();
+                line = GetNextLine(dates);
                 pos++;
             }
 
@@ -186,29 +203,23 @@ namespace ZapReport.Objects
                 {
                     match = LogRegEx.RegexRotations.Match(line);
 
-                    var index = int.Parse(match.Groups[3].Value);
-
-                    _logger.Log(LogLevel.Info, $"Found isocenter {index}");
-
-                    if (!fraction.Isocenters.ContainsKey(index))
-                    {
-                        fraction.Isocenters.Add(index, new LogIsocenterEntry());
-                    }
-
-                    isocenter = fraction.Isocenters[index];
+                    isocenter = new LogIsocenterEntry();
 
                     isocenter.Time = DateTime.ParseExact(match.Groups[1].Value + " " + match.Groups[2].Value, "MM.dd.yyyy HH:mm:ss.FFF", CultureInfo.InvariantCulture);
-                    isocenter.Index = index;
                     isocenter.Rotation_Pitch = double.Parse(match.Groups[4].Value, CultureInfo.InvariantCulture);
                     isocenter.Rotation_Roll = double.Parse(match.Groups[5].Value, CultureInfo.InvariantCulture);
                     isocenter.Rotation_Yaw = double.Parse(match.Groups[6].Value, CultureInfo.InvariantCulture);
                     isocenter.ID = int.Parse(match.Groups[7].Value);
                     isocenter.IsAutoAlignment = match.Groups[8].Value.ToUpper() == "TRUE" ? true : false;
+
+                    fraction.Isocenters.Add(isocenter);
+
+                    _logger.Log(LogLevel.Info, $"Found isocenter with ID {isocenter.ID} and {isocenter.Rotation_Pitch}/{isocenter.Rotation_Roll}/{isocenter.Rotation_Yaw} (P/R/Y) at {isocenter.Time}");
                 }
 
                 if (LogRegEx.RegexMeasured.IsMatch(line) && isocenter != null)
                 {
-                    var beam = GetNextContent(line, ref fraction, ref isocenter);
+                    var beam = GetNextContent(line, dates, ref fraction, ref isocenter);
 
                     if (beam != null)
                     {
@@ -216,6 +227,8 @@ namespace ZapReport.Objects
                         isocenter.TotalBeams++;
                         fraction.TotalBeams++;
                     }
+
+                    _logger.Log(LogLevel.Info, $"Found beam {beam.Node}/{beam.Axial}/{beam.Oblique}/{beam.PlannedMU} at {beam.Time}");
                 }
 
                 if (LogRegEx.RegexIsocenterEnd.IsMatch(line) && isocenter != null)
@@ -224,27 +237,27 @@ namespace ZapReport.Objects
 
                     isocenter.TotalTimeInSeconds = double.Parse(match.Groups[5].Value);
 
-                    _logger.Log(LogLevel.Info, $"Found end of isocenter");
+                    _logger.Log(LogLevel.Info, $"Found end of isocenter with ID {isocenter.ID} at {DateTime.ParseExact(match.Groups[1].Value + " " + match.Groups[2].Value, "MM.dd.yyyy HH:mm:ss.FFF", CultureInfo.InvariantCulture)}");
                 }
 
                 if (LogRegEx.RegexFractionEnd.IsMatch(line))
                 {
-                    _logger.Log(LogLevel.Info, $"End of fraction {fraction.Fraction} of plan \"{fraction.PlanName}\"");
+                    _logger.Log(LogLevel.Info, $"End of fraction {fraction.Fraction} of plan \"{fraction.PlanName}\" found at {DateTime.ParseExact(match.Groups[1].Value + " " + match.Groups[2].Value, "MM.dd.yyyy HH:mm:ss.FFF", CultureInfo.InvariantCulture)}");
 
                     return fraction;
                 }
 
-                line = GetNextLine();
+                line = GetNextLine(dates);
                 pos++;
             }
 
-            _logger.Log(LogLevel.Error, $"No end of fraction found");
+            _logger.Log(LogLevel.Error, $"No end of fraction found. Perhaps a makeup fraction?");
 
-            return null;
+            return fraction;
         }
 
 
-        private LogBeamEntry GetNextContent(string activeLine, ref LogFractionEntry fraction, ref LogIsocenterEntry isocenter)
+        private LogBeamEntry GetNextContent(string activeLine, List<(DateTime, DateTime)> dates, ref LogFractionEntry fraction, ref LogIsocenterEntry isocenter)
         {
             LogBeamEntry beam;
 
@@ -252,7 +265,7 @@ namespace ZapReport.Objects
 
             while (line != null && !LogRegEx.RegexMeasured.IsMatch(line))
             {
-                line = GetNextLine();
+                line = GetNextLine(dates);
             }
 
             if (line == null)
@@ -310,15 +323,15 @@ namespace ZapReport.Objects
                     return beam;
                 }
 
-                line = GetNextLine();
+                line = GetNextLine(dates);
             }
 
             return null;
         }
 
-        private string GetNextLine()
+        private string GetNextLine(List<(DateTime, DateTime)> dates)
         {
-            if (_files == null || _files.Count == 0) 
+            if (_files == null || _files.Count == 0 || _fileIndex >= _files.Count) 
             {
                 return null;
             }
@@ -331,7 +344,7 @@ namespace ZapReport.Objects
                 _activeFile.Open();
             }
 
-            var line = _activeFile?.GetNextLine();
+            var line = _activeFile?.GetNextLine(dates);
 
             // Are there no more lines to check in this file left?
             while (line == null)
@@ -352,27 +365,10 @@ namespace ZapReport.Objects
 
                 _activeFile.Open();
 
-                line = _activeFile.GetNextLine();
+                line = _activeFile.GetNextLine(dates);
             }
 
             return line;
-        }
-
-        private bool CheckDate(string strDate, string strTime, DateTime startTime, DateTime endTime)
-        {
-            string strDateTime = strDate + " " + strTime;
-            DateTime dateTime;
-
-            try
-            {
-                dateTime = DateTime.ParseExact(strDateTime, "MM.dd.yyyy HH:mm:ss.FFF", null);
-            }
-            catch
-            {
-                return false;
-            }
-
-            return startTime <= dateTime && dateTime <= endTime;
         }
     }
 }
